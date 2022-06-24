2516b14568671d624abb3c13ff0dc9a5366edba5
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-06-24 07:48:36 +0530

    [JS] moview data stored in local storage

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -6,17 +6,30 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+// save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex',movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+};
+
 // update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+    localStorage.setItem('selectedSeats', JSON.stringify(seatIndex));
+
+    console.log('seatIndex', seatIndex);
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
-}
+};
 
 // movie select event
 movieSelect.addEventListener('change', (event) => {
-    ticketPrice = +event.target.value;
+    const { value, selectedIndex } = event.target;
+    ticketPrice = +value;
+    setMovieData(selectedIndex, value);
     updateSelectedCount();
 });
 
